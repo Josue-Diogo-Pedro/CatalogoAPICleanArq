10f8dd6848b33e0eb4ad220046ad0db8705ad33a
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 15:46:31 +0100

    Finish to implement CategoriaController

--- Catalogo.API/Controllers/CategoriasController.cs
+++ Catalogo.API/Controllers/CategoriasController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Catalogo.Application.DTOs;
+using Catalogo.Application.Interfaces;
+using Catalogo.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Catalogo.API.Controllers;
 
@@ -6,4 +9,53 @@ namespace Catalogo.API.Controllers;
 [ApiController]
 public class CategoriasController : ControllerBase
 {
+    private readonly ICategoriaService _categoriaService;
+    public CategoriasController(ICategoriaService categoriaService) => _categoriaService = categoriaService;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CategoriaDTO>>> Get()
+    {
+        var categorias = await _categoriaService.GetCategoriaDTOsAsync();
+        return Ok(categorias);
+    }
+
+    [HttpGet("{id}", Name = "GetCategoria")]
+    public async Task<ActionResult<Categoria>> Get(int id)
+    {
+        var categoria = await _categoriaService.GetByIdAsync(id);
+        if(categoria is null) return NotFound();
+
+        return Ok(categoria);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] CategoriaDTO categoriaDTO)
+    {
+        if(!ModelState.IsValid) return BadRequest(ModelState);
+
+        await _categoriaService.AddAsync(categoriaDTO);
+
+        return new CreatedAtRouteResult("GetCategoria", new { id = categoriaDTO.Id });
+    }
+
+    [HttpPut("id")]
+    public async Task<ActionResult> Put(int id, [FromBody]CategoriaDTO categoriaDTO)
+    {
+        if(!ModelState.IsValid) return BadRequest(ModelState);
+
+        if(id != categoriaDTO.Id) return BadRequest();
+
+        await _categoriaService.UpdateAsync(categoriaDTO);
+        return Ok(categoriaDTO);
+    }
+
+    [HttpDelete("id")]
+    public async Task<ActionResult<Categoria>> Delete(int id)
+    {
+        var categoriaDTO = await _categoriaService.GetByIdAsync(id);
+        if(categoriaDTO is null) return NotFound();
+
+        await _categoriaService.RemoveAsync(id);
+        return Ok(categoriaDTO);
+    }
 }
