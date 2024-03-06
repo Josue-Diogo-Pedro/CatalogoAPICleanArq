c840e8502e50f2730d49992c662c71b0341b4c15
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 10:59:13 +0100

    Add method in Categoria to validate properties

--- Catalogo.Domain/Entities/Categoria.cs
+++ Catalogo.Domain/Entities/Categoria.cs
@@ -1,4 +1,6 @@
-﻿namespace Catalogo.Domain.Entities;
+﻿using Catalogo.Domain.Validation;
+
+namespace Catalogo.Domain.Entities;
 
 public sealed class Categoria : Entity
 {
@@ -6,5 +8,15 @@ public sealed class Categoria : Entity
     public string ImagemUrl { get; private set; }
     public ICollection<Produto> Produtos { get; set; }
 
+    private void ValidateDomain(string nome, string imagemUrl)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(nome), "Nome inválido. O nome é obrigatório");
+        DomainExceptionValidation.When(string.IsNullOrEmpty(imagemUrl), "Nome da imagem inválido. O nome é obrigatório");
 
+        DomainExceptionValidation.When(nome.Length < 3, "O nome deve ter no mínimo 3 caracteres");
+        DomainExceptionValidation.When(imagemUrl.Length < 5, "Nome da imagem deve ter no mínimo 5 caracteres");
+
+        Nome = nome;
+        ImagemUrl = imagemUrl;
+    }
 }
