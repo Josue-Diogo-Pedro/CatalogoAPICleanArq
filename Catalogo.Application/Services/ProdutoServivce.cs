20a8796981e4bcc60c6fa97b1ae11514eca70e1a
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 15:17:16 +0100

    Create ProdutoService and implement dependency injection in IoC

---
+++ Catalogo.Application/Services/ProdutoServivce.cs
@@ -0,0 +1,34 @@
+﻿using Catalogo.Application.DTOs;
+using Catalogo.Application.Interfaces;
+
+namespace Catalogo.Application.Services;
+
+public class ProdutoServivce : IProdutoService
+{
+
+    public Task<IEnumerable<ProdutoDTO>> GetProdutoDTOsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProdutoDTO> GetByIdAsync(int? id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(ProdutoDTO produtoDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(ProdutoDTO produtoDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveAsync(int? id)
+    {
+        throw new NotImplementedException();
+    }
+
+}
---
+++ Catalogo.CrossCutting/IoC/DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Catalogo.Application.Interfaces;
+using Catalogo.Application.Mappings;
+using Catalogo.Application.Services;
+using Catalogo.Domain.Interfaces;
+using Catalogo.Infrastructure.Context;
+using Catalogo.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalogo.CrossCutting.IoC;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+        services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+        services.AddScoped<IProdutoService, ProdutoServivce>();
+        services.AddScoped<ICategoriaService, CategoriaService>();
+
+        var mappingConfig = new MapperConfiguration(mc =>
+        {
+            mc.AddProfile(new DomainToDTOMappingProfile());
+        });
+
+        IMapper mapper = mappingConfig.CreateMapper();
+        services.AddSingleton(mapper);
+
+        return services;
+    }
+}
