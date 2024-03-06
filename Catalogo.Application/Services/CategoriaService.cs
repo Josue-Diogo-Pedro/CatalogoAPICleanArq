97519ec13886acc157caf25f3d96c99fa19083b9
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 12:43:04 +0100

    Add CategoriaService class and put him to inherit ICategoriaService

---
+++ Catalogo.Application/Services/CategoriaService.cs
@@ -0,0 +1,32 @@
+﻿using Catalogo.Application.DTOs;
+using Catalogo.Application.Interfaces;
+
+namespace Catalogo.Application.Services;
+
+public class CategoriaService : ICategoriaService
+{
+    public Task<IEnumerable<CategoriaDTO>> GetCategoriaDTOsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CategoriaDTO> GetByIdAsync(int? id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(CategoriaDTO categoriaDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(CategoriaDTO categoriaDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveAsync(int? id)
+    {
+        throw new NotImplementedException();
+    }
+}
