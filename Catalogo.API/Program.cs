bca6e88cb634d3e7ec53f13ce042020e61f4c8d2
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 15:33:22 +0100

    Add services and middlewares in program

--- Catalogo.API/Program.cs
+++ Catalogo.API/Program.cs
@@ -1,11 +1,22 @@
+using Catalogo.CrossCutting.IoC;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalogo.API", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -13,7 +24,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalogo.API v1"));
 }
 
 app.UseHttpsRedirection();
