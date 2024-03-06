a504abeaf39dbebc9671e325451f137ec1aaceea
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 14:13:33 +0100

    Implement configurations in ApplicationDbContext

--- Catalogo.Infrastructure/Context/ApplicationDbContext.cs
+++ Catalogo.Infrastructure/Context/ApplicationDbContext.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Catalogo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Catalogo.Infrastructure.Context;
 
 public class ApplicationDbContext : DbContext
 {
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
 }
