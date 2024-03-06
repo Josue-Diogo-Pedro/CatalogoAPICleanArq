bbf0bd0cdcaeb7f4d0d136dafab7934a807317da
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 11:44:19 +0100

    Add CategoriaDTO - Validation in front -end

---
+++ Catalogo.Application/DTOs/CategoriaDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalogo.Application.DTOs;
+
+public class CategoriaDTO
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Informe o nome da categoria")]
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "Informe o nome da imagem")]
+    [MinLength(5)]
+    [MaxLength(250)]
+    public string ImagemUrl { get; set; }
+}
