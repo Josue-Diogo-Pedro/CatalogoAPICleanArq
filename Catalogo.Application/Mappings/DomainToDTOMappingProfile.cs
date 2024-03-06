9df20d91891c25b240b98401c88110dc7a529f7f
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 12:18:55 +0100

    Add mapping Domain to DTO and DTO to Domain

---
+++ Catalogo.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Catalogo.Application.DTOs;
+using Catalogo.Domain.Entities;
+
+namespace Catalogo.Application.Mappings;
+
+public class DomainToDTOMappingProfile : Profile
+{
+    public DomainToDTOMappingProfile()
+    {
+        CreateMap<Categoria, CategoriaDTO>().ReverseMap();
+        CreateMap<Produto, ProdutoDTO>().ReverseMap();
+    }
+}
