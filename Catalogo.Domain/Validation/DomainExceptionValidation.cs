4225f89c2932f6268449c657ee79112964b4e4a8
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 10:40:52 +0100

    Add folder validation and implementation of DomainExceptionValidation

---
+++ Catalogo.Domain/Validation/DomainExceptionValidation.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalogo.Domain.Validation;
+
+public class DomainExceptionValidation : Exception
+{
+    public DomainExceptionValidation(string error) : base(error) { }
+
+    public static void When(bool hasError, string error)
+    {
+        if (hasError) throw new DomainExceptionValidation(error);
+    }
+}
