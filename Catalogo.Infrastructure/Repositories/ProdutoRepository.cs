8e841e0a2af409abc8c47f82b4f8cb32662b20fd
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 14:47:03 +0100

    Create ProdutoRepository class

---
+++ Catalogo.Infrastructure/Repositories/ProdutoRepository.cs
@@ -0,0 +1,32 @@
+﻿using Catalogo.Domain.Entities;
+using Catalogo.Domain.Interfaces;
+
+namespace Catalogo.Infrastructure.Repositories;
+
+public class ProdutoRepository : IProdutoRepository
+{
+    public Task<Produto> CreateAsync(Produto produto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Produto> GetByIdAsync(int? id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Produto>> GetProdutosAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Produto> RemoveAsync(Produto produto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Produto> UpdateAsync(Produto produto)
+    {
+        throw new NotImplementedException();
+    }
+}
