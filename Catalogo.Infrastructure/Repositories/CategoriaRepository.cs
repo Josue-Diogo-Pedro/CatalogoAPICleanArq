8794d21c99b45983ac1f793903ee12663f78aca6
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 14:43:56 +0100

    Finish to implement CategoriaRepository

--- Catalogo.Infrastructure/Repositories/CategoriaRepository.cs
+++ Catalogo.Infrastructure/Repositories/CategoriaRepository.cs
@@ -1,6 +1,41 @@
-﻿namespace Catalogo.Infrastructure.Repositories;
+﻿using Catalogo.Domain.Entities;
+using Catalogo.Domain.Interfaces;
+using Catalogo.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
-public class CategoriaRepository
+namespace Catalogo.Infrastructure.Repositories;
+
+public class CategoriaRepository : ICategoriaRepository
 {
+    private ApplicationDbContext _context;
+    public CategoriaRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
 
+    public async Task<IEnumerable<Categoria>> GetCategoriasAsync()
+        => await _context.Categorias.AsNoTracking().DefaultIfEmpty().ToListAsync();
+
+    public async Task<Categoria> GetByIdAsync(int? id)
+        => await _context.Categorias.FindAsync(id);
+
+    public async Task<Categoria> CreateAsync(Categoria categoria)
+    {
+        _context.Categorias.Add(categoria);
+        await _context.SaveChangesAsync();
+        return categoria;
+    }
+
+    public async Task<Categoria> UpdateAsync(Categoria categoria)
+    {
+        _context.Categorias.Update(categoria);
+        await _context.SaveChangesAsync();
+        return categoria;
+    }
+    public async Task<Categoria> RemoveAsync(Categoria categoria)
+    {
+        _context.Categorias.Remove(categoria);
+        await _context.SaveChangesAsync();
+        return categoria;
+    }
 }
