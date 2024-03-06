a935035db321a9a3f6f0c4259a12a5be14510f47
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 14:05:03 +0100

    Add configuration for Categoria and populate same datas

---
+++ Catalogo.Infrastructure/EntitiesConfiguration/CategoriaConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Catalogo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Catalogo.Infrastructure.EntitiesConfiguration;
+
+public class CategoriaConfiguration : IEntityTypeConfiguration<Categoria>
+{
+    public void Configure(EntityTypeBuilder<Categoria> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(p => p.Nome).HasMaxLength(100).IsRequired();
+        builder.Property(p => p.ImagemUrl).HasMaxLength(100).IsRequired();
+
+        builder.HasData(
+            new Categoria(1, "Marial Escolar", "material.jpg"),
+            new Categoria(2, "Eletrônicos", "eletronicos.jpg"),
+            new Categoria(2, "Acessórios", "acessorios.jpg")
+            );
+    }
+}
