8a075921968dc8eb243ce2e8bfb0d7c5091e64ca
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 11:37:45 +0100

    Add interface IProdutoRepository and his contracts

---
+++ Catalogo.Domain/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,12 @@
+﻿using Catalogo.Domain.Entities;
+
+namespace Catalogo.Domain.Interfaces;
+
+public interface IProdutoRepository
+{
+    Task<IEnumerable<Produto>> GetProdutosAsync();
+    Task<Produto> GetByIdAsync(int? id);
+    Task<Produto> CreateAsync(Produto produto);
+    Task<Produto> UpdateAsync(Produto produto);
+    Task<Produto> RemoveAsync(Produto produto);
+}
