d9e17e332cae1fdd86dc2d0df9a572097871a16f
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 11:04:39 +0100

    Add properties in Produto

--- Catalogo.Domain/Entities/Produto.cs
+++ Catalogo.Domain/Entities/Produto.cs
@@ -1,5 +1,11 @@
 ﻿namespace Catalogo.Domain.Entities;
 
-public class Produto
+public sealed class Produto : Entity
 {
+    public string Nome { get; private set; }
+    public string Descricao { get; private set; }
+    public decimal Preco { get; private set; }
+    public string ImagemUrl { get; private set; }
+    public int Estoque { get; private set; }
+    public DateTime DataCadastro { get; private set; }
 }
