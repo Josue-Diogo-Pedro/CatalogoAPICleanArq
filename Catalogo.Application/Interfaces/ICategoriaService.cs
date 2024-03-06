36f82abf542ff254ebf81d3f2ae30baeba291ee8
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 12:35:01 +0100

    Add ICategoriaService and implement his contract

---
+++ Catalogo.Application/Interfaces/ICategoriaService.cs
@@ -0,0 +1,12 @@
+﻿using Catalogo.Application.DTOs;
+
+namespace Catalogo.Application.Interfaces;
+
+public interface ICategoriaService
+{
+    Task<IEnumerable<CategoriaDTO>> GetCategoriaDTOsAsync();
+    Task<CategoriaDTO> GetByIdAsync(int? id);
+    Task AddAsync(CategoriaDTO categoriaDTO);
+    Task UpdateAsync(CategoriaDTO categoriaDTO);
+    Task RemoveAsync(int? id);
+}
