f892db189dd5474de0bc882ff5d4c392035851d8
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 11:34:13 +0100

    Add interface ICategoriaRepository and his contracts

---
+++ Catalogo.Domain/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,12 @@
+﻿using Catalogo.Domain.Entities;
+
+namespace Catalogo.Domain.Interfaces;
+
+public interface ICategoriaRepository
+{
+    Task<IEnumerable<Categoria>> GetCategoriasAsync();
+    Task<Categoria> GetByIdAsync(int? id);
+    Task<Categoria> CreateAsync(Categoria categoria);
+    Task<Categoria> UpdateAsync(Categoria categoria);
+    Task<Categoria> RemoveAsync(Categoria categoria);
+}
