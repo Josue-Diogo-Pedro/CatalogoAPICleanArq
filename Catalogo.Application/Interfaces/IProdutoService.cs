1184d04703327187c50d5aa2026c3e998595c556
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 12:38:37 +0100

    Add IProdutoService and his contract

---
+++ Catalogo.Application/Interfaces/IProdutoService.cs
@@ -0,0 +1,12 @@
+﻿using Catalogo.Application.DTOs;
+
+namespace Catalogo.Application.Interfaces;
+
+public interface IProdutoService
+{
+    Task<IEnumerable<ProdutoDTO>> GetProdutoDTOsAsync();
+    Task<ProdutoDTO> GetByIdAsync(int? id);
+    Task AddAsync(ProdutoDTO produtoDTO);
+    Task UpdateAsync(ProdutoDTO produtoDTO);
+    Task RemoveAsync(int? id);
+}
