22a5a96d4cc7a40ba7fc0c0547d5402c32b36548
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-06 12:16:34 +0100

    Add ProdutoDTO and his front-end validation

---
+++ Catalogo.Application/DTOs/ProdutoDTO.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Catalogo.Application.DTOs;
+
+public class ProdutoDTO
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "A descrição é obrigatória")]
+    [MinLength(5)]
+    [MaxLength(100)]
+    public string Descricao { get; set; }
+
+    [Required(ErrorMessage = "Informe o preço")]
+    [Column(TypeName = "decimal(18,2)")]
+    [DisplayFormat(DataFormatString = "{0:C2}")]
+    [DataType(DataType.Currency)]
+    public decimal Preco { get; set; }
+
+    [MaxLength(250)]
+    public string ImagemUrl { get; set; }
+
+    [Required(ErrorMessage = "O estoque é obrigatório")]
+    [Range(1, 9999)]
+    public int Estoque { get; set; }
+
+    [Required(ErrorMessage = "Informe a data do cadastro")]
+    public DateTime DataCadastro { get; set; }
+
+    public int CategoriaId { get; set; }
+}
